bed29301fd5a2597293c45a018d057c5fa6c0541
NandaScott <nanda1123@gmail.com>
2021-04-30 21:00:29 -0400

    Tweaked imports

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -3,12 +3,12 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
+import { currentUserRouter } from '@routes/current-user';
+import { signinRouter } from '@routes/signin';
+import { signoutRouter } from '@routes/signout';
+import { signupRouter } from '@routes/signup';
+import { errorHandler } from '@middlewares/error-handler';
+import { NotFoundError } from '@errors/not-found-error';
 
 const app = express();
 app.set('trust proxy', true); // Allows https with ingress-nginx
