7110d07990cf3ad8000fc9cf9f9bbac7fdffbcbe
NandaScott <nanda1123@gmail.com>
2021-05-02 22:36:46 -0400

    Quick comment

--- client/api/buildClient.js
+++ client/api/buildClient.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export default function buildClient({ req }) {
   if (typeof window === 'undefined') {
     // We are on the server
+    // servicename.namespacename.svc.cluster.local
     return axios.create({
       baseURL:
         'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
